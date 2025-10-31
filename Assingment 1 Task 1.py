8ec95ea6d2457dd9aa95a4d6c4936e46133778df
mrkanishk100 <mrkanishkrajput82@gmail.com>
2025-10-31 12:48:27 +0530

    Add files via upload

---
+++ Assingment 1 Task 1.py
@@ -0,0 +1,18 @@
+a=int(input('Enter First Number: '))
+b=int(input('Enter Second Number: '))
+
+#Addition
+
+print('Addition:',a+b)
+
+#Subtraction
+
+print('Subtraction:',a-b)
+
+#Multiplication
+
+print('Multiplication:',a*b)
+
+#Division
+
+print('Division:',a/b)
---
+++ Assingment 1 Task 2.py
@@ -0,0 +1,8 @@
+#Taking Inputs
+a=str(input('Enter your First Name: '))
+b=str(input('Enter your Second Name: '))
+
+#Concatation
+c=a+' '+b+'!'
+
+print('Hello,',c,'Welcome to the Python program.')
